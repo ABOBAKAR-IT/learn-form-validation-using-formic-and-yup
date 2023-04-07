bd66ff6f9aa535c8bf76eca950cf910c5e4c74b8
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-04-07 16:46:34 +0500

    RTK done

--- src/Redux/Slice/registerSlice.jsx
+++ src/Redux/Slice/registerSlice.jsx
@@ -1,13 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    full_name:"",
+   person:{
+    full_name:"tayyab",
     user_name:"",
     phone_no:"",
     email:"",
     cnic:"",
     type:"",
     password:""
+   }
 }
 
 const registerSlice = createSlice({
@@ -15,29 +17,33 @@ const registerSlice = createSlice({
     initialState,
     reducers: {
         setFullName: (state, action) => {
-            state.full_name = action.payload;
+            state.person.full_name = action.payload;
         },
         setUserName: (state, action) => {
-            state.user_name = action.payload;
+            state.person.user_name = action.payload;
         },
         setPhoneNo: (state, action) => {
-            state.phone_no = action.payload;
+            state.person.phone_no = action.payload;
         },
         setEmail: (state, action) => {
-            state.email = action.payload;
+            state.person.email = action.payload;
         },
         setCnic: (state, action) => {
-            state.cnic = action.payload;
+            state.person.cnic = action.payload;
         },
         setType: (state, action) => {
-            state.type = action.payload;
+            state.person.type = action.payload;
         },
         setPassword: (state, action) => {
-            state.password = action.payload;
+            state.person.password = action.payload;
+        },
+        setData:(state,action)=>{
+          console.log(action.payload)
+   state.person=action.payload
         }
     
         }}
         )
 
-export const { setFullName, setUserName, setPhoneNo, setEmail, setCnic, setType, setPassword } = registerSlice.actions;
+export const { setFullName, setUserName, setPhoneNo, setEmail, setCnic, setType, setPassword,setData } = registerSlice.actions;
 export default registerSlice.reducer;
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import registerSlice from "./Slice/registerSlice";
 const store = configureStore({
     reducer: {
-        // Add your reducers here       
+       register:registerSlice 
 }
 });
+export default store
